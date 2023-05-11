a69ab34947f44e716a30c54bfa1db5f56632b1cb
knightfall22 <pelumi066@gmail.com>
2023-05-11 09:59:14 +0100

    minor design changes

---
+++ js/profile.js
@@ -0,0 +1,59 @@
+export const profiles = [
+  `"My name is Olukunle Ayodele JP. I grew up at the Oju-Irin area of Ago Tailor, Odo-Ona Ibadan. I attended primary and secondary schools education in Ibadan. I was privileged to attend Ogun State University for my BA degree and obtained a Postgraduate diploma in Theology in London. I am one of the senior pastors in the Redeemed Christian Church Of God in London where I am currently based with my family. I was also privileged to be the pioneer president of Agala Klub 12. We were all young boys who grew up together. Our focus then was purely social and togetherness. I believe it was the same spirit of togetherness that has helped us to revive it again."`,
+
+  `"My name is Adesoji Oyedele. I grew up at Oju Irin area, off Desalu Street. Had my elementary school at the famous C.K.C, class of 1970. Started my secondary education in Ibadan, but completed it in Igbajo. Joined the service of the old Oyo, as a Clerical Officer, in 1978. Got a scholarship to study History & International Studies in UNAD(1997 - 2001). Disengaged from the public service of Osun State, as a Deputy Director, in 2008. Relocated to the Republic of Ireland in 2009. Obtained a master's degree in public administration, and a master's degree in business administration from the prestigious Dublin Business School, in 2011 & 2013 respectively. I work in the HR department of the HSE, Dublin. I am married, with kids and grandkids."`,
+
+  null,
+
+  null,
+
+  null,
+
+  null,
+
+  `"My name is Timothy Kayode Babarimisa Fcib, Ficcm,  Mcfin.  I grew up in Ago Taylor and my family's house address is at Sw9/312 B, Railway line, Ago Taylor, Odo Ona, Ibadan.
+    Presently l am the managing partner of Timrock Professional Services, a company that specialises in digital and  computer forensic investigation, fraud investigation, consultancy,  Training, Debt recovery agent, business advisory,  etc. Its head office is in No 4 Ifelodun Quarter, Ajara, Olorunda Abba Road, lbadan.
+    Also l am the Managing Director of Integrated Farmer's Shop Ltd; a company that deals in animal feeds. The warehouse is at the ground floor of a storey building at No 4 Ifelodun Quarters, Ajara, Olorunda Abba Road, lbadan."`,
+
+  `"My name is Lawson Oluwole Oyeniyi. My nickname before is LEO.
+    I attended CKC primary school and later attended Baptist Day School Ijagbo - Offa, and Offa Grammar School all in Kwara State. Studied Printing at London College of Printing, now London University of Communication. 
+    Worked with different printing companies, now a Pastor with Faith Clinic Nigeria Incorporated, Ijokodo junction, Ibadan.
+    I'm part of those who started the KLUB and serves as the pioneer secretary and also serving now as the reunion secretary"`,
+
+  `"My name is Tunji Mukaram  Soroye. 
+    I grew up at Ago - Taylor, Odo-Ona, ibadan.
+    My family's house address is Sw9/436c Desalu street, Ago Taylor Odo-Ona, Ibadan. Educational Background: 1:C.K.C. pry. Sch.odoona,ib, 2.Egba Odeda High school Abeokuta ,Ogun State.3.Technical  College,Ijebu    Ode ,Ogun State.Qualificatioñ Obtained 1:Pry.Sch.Certificate,2.W.A.E.C.certificate.3.National Diploma (ND ) ,(Electrical Electronics).
+    Working. Experience 1.N.E.P.A   (Engr.Dept.)2.Latmor Comp.(Cashier,/Store Keeper,).3.Kohfam Ltd.(Cashier/Store Keeper)4.
+    Transporter. Now Retired and venture into farming(Crop Production.) also a  Contractor in building project."`,
+
+  `"My name is Olabode Kehinde Kolawole. I was born and grew up in Ago Taylor Oju irin Ibadan. My family house address is SW9/610 Ago Taylor Ibadan. I schooled at C.A.C primary School,  Odo Ona   Ibadan. I am one of the founding member of the Klub and a former Vice president of the Klub.  Presently I am based in United Kingdom."`,
+
+  `"My name is Engr. Bola Olowe FNSE, FNIEE. I grew up in Olowe Lane, Ago - Taylor and my family’s house address is SW9/440, Olowe Lane, Ago – Tailor. Presently I am the Managing Director of Bolcon Associates Ltd, a company that specializes in Project Management, Engineering Consulting, Training, Research and Development. Our office is at Fundex Building, Behind Glory Tabernacle Church, Bodija – Ashi Road, Bodija, Ibadan. I am also the President of Grace Leadership Foundation, a Non-Governmental, Faith-based Organization with the Vision of Raising the Foundations of Many Generations, through the provision of leadership training and development, mentoring and sponsorship of our youths"`,
+
+  null,
+
+  `"My name is SIKIRU KOLAWOLE KADIRI. I was born and grew up at SW9/659A, Desalu Street, Odo Ona, lbadan. I went to CKC Primary School, Odo Ona and high school at Anglican Secondary Modern School, at  Orita Mefa, lbadan, now Anglican Secondary School, later had my WAEC A levels, Diploma in Salesmanship at UK long distance studies and proceeded to learn Electronics engineering at lbadan. I later joined Eko Hotels & Suites, Victoria lsland, Lagos and retired as a Business Supervisor. Presently l am into Hotel Business Consultant and selling of imported Electronics appliances. My present address is at No 31 Association Avenue, Folarin Estate, ltoki, Ogun State."`,
+
+  `"My name is Alhaji Saliu Orunsolu
+    I attended CKC primary School Odo-Ona, i latter  went  to Nawarudeen Grammar School, Abeokuta. 
+   I attended Ogun State Poly.
+   I work at CFAO.
+   I am presently self employed,  I deal in selling and repairing of photocopying machines. I also sell building materials "`,
+
+  `"My name is Olufemi Fadumila,  family house address is SW9/799. I grew up at 
+   Oju‐Irin Ago-Taylor, Odo-Ona lbadan.
+    Attended my Primary school at St James Cathedral Oke Bola and Secondary school education at African Church Grammar School Apata Ganga.
+   Attended school ofPrrinting and Graphic Art, Yaba. 
+   C.E.O Fadmart Int.Nig.Ltd. but presently C.E.O Fadmart Block Industry lagos, importer and exporter."`,
+
+  null,
+
+  `"My name is FADUMILA AYODELE, I was born in ibadan and grew up at oju irin ,agala Area. I went to CKC primary school and odo ona high school for my post primary.I further to Federal poly Ado Ekiti for my ND and HND in business administration and mgt studies.i worked at AMSEK oil and gas and TESSY Energy LTD as a manager.  Presently , running my business on oil and gas business under STK PRUDENT MULTICONCEPT NIG LTD as a director." `,
+
+  `"My name is Olasunkanmi Tejuoso born into the family of Pa Ogunbunmi Tejuoso of the famous baba carpenter at Olowe Lane and grew up at SW9/ 309 Railway line Ago Tailor Odo Ona, i am a product of CKC Primary School, Odo Ona, Ibadan, St Leo's High School, Abeokuta and Plateau State Polytechnic,  Jos
+  Presently, I am a Pastor at The Living Christ Evangelical Ministries
+  I joined this Club on 17th of May 2021"`,
+
+  `"My name is Olayinka Tejuosho. I lived and grew up at SW9/309 Ago Taylor Street, Odo Ona lbadan. I attended the CKC Primary School, Odo Ona, Alafia institute Mokola, Government College Ibadan and the University of Lagos with degrees in Architecture, Town Planning and Project Management. I retired as a Deputy Director at the Federal Ministry of Environment, Abuja in 2019. I currently reside in the United States."`,
+
+];
--- js/script.js
+++ js/script.js
@@ -1,10 +1,49 @@
+import { profiles } from "./profile";
 const container = [...document.querySelectorAll('.members__group')];
 const next = [...document.querySelectorAll('.next')];
 const prev = [...document.querySelectorAll('.prev')];
 
 const mobileNavs = [...document.querySelectorAll('.mobile__navigation__link')];
 const nav_toggle = document.getElementById('navi-toogle');
-console.log(next);
+
+const boxes = document.querySelectorAll('.members__box')
+const modal = document.getElementById('modal')
+const outputDiv = document.getElementById('modal__paragraph')
+const modal__image = document.getElementById('modal__image')
+
+modal.addEventListener('click', () => {
+    outputDiv.innerHTML = ``
+    modal.classList.remove('visible')
+})
+
+boxes.forEach((item, i) => {
+
+    item.addEventListener('click', () => {
+        if (profiles[i]) {
+            modal.classList.add('visible')
+        
+            const img = {...item.getElementsByTagName('img')}
+            const imgSrc = img[0].src
+    
+            modal__image.src = imgSrc
+            let textIndex = 0;
+            outputDiv.innerHTML = ``
+            const intervalId =  setInterval(function () {
+                outputDiv.innerHTML += profiles[i][textIndex]
+                textIndex++;
+                if (textIndex === profiles[i].length  || !modal.classList.contains('visible')) {
+                    clearInterval(intervalId)
+                }
+            }, 40)
+        }
+        
+    })
+})
+
+
+
+
+
 
 mobileNavs.forEach((item, i) => {
     item.addEventListener('click', () => {
