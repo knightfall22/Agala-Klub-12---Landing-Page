0fe80e604d8db4a8b13fc147355b499912d6edec
knightfall22 <pelumi066@gmail.com>
2023-09-25 17:13:53 +0100

    Minor Adjustments

--- js/profile.js
+++ js/profile.js
@@ -3,17 +3,16 @@ export const profiles = [
 
   `"My name is Adesoji Oyedele. I grew up at Oju Irin area, off Desalu Street. Had my elementary school at the famous C.K.C, class of 1970. Started my secondary education in Ibadan, but completed it in Igbajo. Joined the service of the old Oyo, as a Clerical Officer, in 1978. Got a scholarship to study History & International Studies in UNAD(1997 - 2001). Disengaged from the public service of Osun State, as a Deputy Director, in 2008. Relocated to the Republic of Ireland in 2009. Obtained a master's degree in public administration, and a master's degree in business administration from the prestigious Dublin Business School, in 2011 & 2013 respectively. I work in the HR department of the HSE, Dublin. I am married, with kids and grandkids."`,
 
-  null,
-
-  null,
-
-  null,
-
-  null,
-
   `"My name is Timothy Kayode Babarimisa Fcib, Ficcm,  Mcfin.  I grew up in Ago Taylor and my family's house address is at Sw9/312 B, Railway line, Ago Taylor, Odo Ona, Ibadan.
     Presently l am the managing partner of Timrock Professional Services, a company that specialises in digital and  computer forensic investigation, fraud investigation, consultancy,  Training, Debt recovery agent, business advisory,  etc. Its head office is in No 4 Ifelodun Quarter, Ajara, Olorunda Abba Road, lbadan.
     Also l am the Managing Director of Integrated Farmer's Shop Ltd; a company that deals in animal feeds. The warehouse is at the ground floor of a storey building at No 4 Ifelodun Quarters, Ajara, Olorunda Abba Road, lbadan."`,
+  null,
+
+  null,
+
+  null,
+
+  null,
 
   `"My name is Lawson Oluwole Oyeniyi. My nickname before is LEO.
     I attended CKC primary school and later attended Baptist Day School Ijagbo - Offa, and Offa Grammar School all in Kwara State. Studied Printing at London College of Printing, now London University of Communication. 
@@ -30,48 +29,50 @@ export const profiles = [
 
   `"My name is Engr. Bola Olowe FNSE, FNIEE. I grew up in Olowe Lane, Ago - Taylor and my family’s house address is SW9/440, Olowe Lane, Ago – Tailor. Presently I am the Managing Director of Bolcon Associates Ltd, a company that specializes in Project Management, Engineering Consulting, Training, Research and Development. Our office is at Fundex Building, Behind Glory Tabernacle Church, Bodija – Ashi Road, Bodija, Ibadan. I am also the President of Grace Leadership Foundation, a Non-Governmental, Faith-based Organization with the Vision of Raising the Foundations of Many Generations, through the provision of leadership training and development, mentoring and sponsorship of our youths"`,
 
-  `"My name is Mr Adeniyi George Soremekun, from the  Soremekun family at SW9/534,Fagbemi Street, Odo-Ona,  Ibadan. 
-  I attended at C.K.C primary School and also Motailatu College both in Odo-Ona. 
-  I am the CEO of Ganifield Tailoring and Fashion Designing."`,
-
-  `"My name is SIKIRU KOLAWOLE KADIRI. I was born and grew up at SW9/659A, Desalu Street, Odo Ona, lbadan. I went to CKC Primary School, Odo Ona and high school at Anglican Secondary Modern School, at  Orita Mefa, lbadan, now Anglican Secondary School, later had my WAEC A levels, Diploma in Salesmanship at UK long distance studies and proceeded to learn Electronics engineering at lbadan. I later joined Eko Hotels & Suites, Victoria lsland, Lagos and retired as a Business Supervisor. Presently l am into Hotel Business Consultant and selling of imported Electronics appliances. My present address is at No 31 Association Avenue, Folarin Estate, ltoki, Ogun State."`,
-
-  `"My name is Alhaji Saliu Orunsolu
-    I attended CKC primary School Odo-Ona, i latter  went  to Nawarudeen Grammar School, Abeokuta. 
-   I attended Ogun State Poly.
-   I work at CFAO.
-   I am presently self employed,  I deal in selling and repairing of photocopying machines. I also sell building materials "`,
-
   `"My name is Olufemi Fadumila,  family house address is SW9/799. I grew up at 
    Oju‐Irin Ago-Taylor, Odo-Ona lbadan.
     Attended my Primary school at St James Cathedral Oke Bola and Secondary school education at African Church Grammar School Apata Ganga.
    Attended school ofPrrinting and Graphic Art, Yaba. 
    C.E.O Fadmart Int.Nig.Ltd. but presently C.E.O Fadmart Block Industry lagos, importer and exporter."`,
 
-  null,
+  `"My name is Mr Adeniyi George Soremekun, from the  Soremekun family at SW9/534,Fagbemi Street, Odo-Ona,  Ibadan. 
+  I attended at C.K.C primary School and also Motailatu College both in Odo-Ona. 
+  I am the CEO of Ganifield Tailoring and Fashion Designing."`,
+
+  `"My name is SIKIRU KOLAWOLE KADIRI. I was born and grew up at SW9/659A, Desalu Street, Odo Ona, lbadan. I went to CKC Primary School, Odo Ona and high school at Anglican Secondary Modern School, at  Orita Mefa, lbadan, now Anglican Secondary School, later had my WAEC A levels, Diploma in Salesmanship at UK long distance studies and proceeded to learn Electronics engineering at lbadan. I later joined Eko Hotels & Suites, Victoria lsland, Lagos and retired as a Business Supervisor. Presently l am into Hotel Business Consultant and selling of imported Electronics appliances. My present address is at No 31 Association Avenue, Folarin Estate, ltoki, Ogun State."`,
+
+  `"My name is Olayinka Tejuosho. I lived and grew up at SW9/309 Ago Taylor Street, Odo Ona lbadan. I attended the CKC Primary School, Odo Ona, Alafia institute Mokola, Government College Ibadan and the University of Lagos with degrees in Architecture, Town Planning and Project Management. I retired as a Deputy Director at the Federal Ministry of Environment, Abuja in 2019. I currently reside in the United States."`,
+
   `"My name is  Babajide Olumuyiwa Adamolekun,  I was born and grew up in ibadan, precisely, Fatimo, Railway Line. My family's home address is; SW9/308A, Railway Line, AgoTaylor, Odo-Ona, Ibadan.
+ 
+   I went to I.M.G, Apata for my primary education and also went to Apata Community Grammar School for my Secondary education. I went to the Federal Polytechnic Ede for both my N.D and HND program.
+   I am the owner of O'SIX GAS, I deal with refilling of different sizes of Gas cylinders, 24/7 home delivery to regular customers, selling of cooking gas cylinder accessories. Also a farmer.
+   My email addresses are: jerry4mum@gmail.com  and jideadams@gmail.com 
+   My mobile phone numbers are 09070681304, 08068474930.
+   I joined this club, Agala Klub 12, on the 17th of May, 2021."`,
 
-  I went to I.M.G, Apata for my primary education and also went to Apata Community Grammar School for my Secondary education. I went to the Federal Polytechnic Ede for both my N.D and HND program.
-  I am the owner of O'SIX GAS, I deal with refilling of different sizes of Gas cylinders, 24/7 home delivery to regular customers, selling of cooking gas cylinder accessories. Also a farmer.
-  My email addresses are: jerry4mum@gmail.com  and jideadams@gmail.com 
-  My mobile phone numbers are 09070681304, 08068474930.
-  I joined this club, Agala Klub 12, on the 17th of May, 2021."`,
-
-  `"My name is FADUMILA AYODELE, I was born in ibadan and grew up at oju irin ,agala Area. I went to CKC primary school and odo ona high school for my post primary.I further to Federal poly Ado Ekiti for my ND and HND in business administration and mgt studies.i worked at AMSEK oil and gas and TESSY Energy LTD as a manager.  Presently , running my business on oil and gas business under STK PRUDENT MULTICONCEPT NIG LTD as a director." `,
+  `"My name is Olusola Ogungbade of
+   Sw9/303, Railway line, Odo -ona, Ibadan.
+    Education -  CKC Pry School, Odo -ona, 
+    Oke'badan High school, Oluyoro Oke- Offa, Ibadan , Ajebo High school, Ajebo, Abeokuta.
+     I studied English language and Music at the Federal College of Education, Abeokuta. 
+     I also studied English language and Russian at the University of Ibadan.
+    I proceeded to the Pushkin Institute, Moscow where I studied Philology. 
+    I also hold a Master's degree in European studies from the University of Ibadan.
+      I am a school Adminstrator, I sell Paints and I paint.
+     I am a full fledged family man, a  grandfather ,  a liberal Christian, I have been privileged to travel to some countries in Africa and Europe and I speak some International languages. I live in Ibadan."`,
 
   `"My name is Olasunkanmi Tejuoso born into the family of Pa Ogunbunmi Tejuoso of the famous baba carpenter at Olowe Lane and grew up at SW9/ 309 Railway line Ago Tailor Odo Ona, i am a product of CKC Primary School, Odo Ona, Ibadan, St Leo's High School, Abeokuta and Plateau State Polytechnic,  Jos
   Presently, I am a Pastor at The Living Christ Evangelical Ministries
   I joined this Club on 17th of May 2021"`,
 
-  `"My name is Olayinka Tejuosho. I lived and grew up at SW9/309 Ago Taylor Street, Odo Ona lbadan. I attended the CKC Primary School, Odo Ona, Alafia institute Mokola, Government College Ibadan and the University of Lagos with degrees in Architecture, Town Planning and Project Management. I retired as a Deputy Director at the Federal Ministry of Environment, Abuja in 2019. I currently reside in the United States."`,
-  `"My name is Olusola Ogungbade of
-  Sw9/303, Railway line, Odo -ona, Ibadan.
-   Education -  CKC Pry School, Odo -ona, 
-   Oke'badan High school, Oluyoro Oke- Offa, Ibadan , Ajebo High school, Ajebo, Abeokuta.
-    I studied English language and Music at the Federal College of Education, Abeokuta. 
-    I also studied English language and Russian at the University of Ibadan.
-   I proceeded to the Pushkin Institute, Moscow where I studied Philology. 
-   I also hold a Master's degree in European studies from the University of Ibadan.
-     I am a school Adminstrator, I sell Paints and I paint.
-    I am a full fledged family man, a  grandfather ,  a liberal Christian, I have been privileged to travel to some countries in Africa and Europe and I speak some International languages. I live in Ibadan."`,
+  `"My name is FADUMILA AYODELE, I was born in ibadan and grew up at oju irin ,agala Area. I went to CKC primary school and odo ona high school for my post primary.I further to Federal poly Ado Ekiti for my ND and HND in business administration and mgt studies.i worked at AMSEK oil and gas and TESSY Energy LTD as a manager.  Presently , running my business on oil and gas business under STK PRUDENT MULTICONCEPT NIG LTD as a director." `,
+
+  `"My name is Alhaji Saliu Orunsolu
+  I attended CKC primary School Odo-Ona, i latter  went  to Nawarudeen Grammar School, Abeokuta. 
+ I attended Ogun State Poly.
+ I work at CFAO.
+ I am presently self employed,  I deal in selling and repairing of photocopying machines. I also sell building materials "`,
+
+  null,
 ];
